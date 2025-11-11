bb95106c431a11e9bcd87e4be3edba14784c3bae
Kelvin Ngumo <kelvinngumo@gmail.com>
2025-11-11 11:46:51 +0300

    New version

--- index calc code.py
+++ index calc code.py
@@ -1,26 +1,50 @@
+def Addition(a, b):
+    return a + b
+
+def subdcription(a, b):
+    return a - b
+
+def  multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return 'Error! Division by zero.'
+    else:
+        return a / b
+
+print('List of available operations:')
 print('1. Addition (+)')
 print('2. Subtraction (-)')
 print('3. Multiplication (*)')
 print('4. Division (/)')
+print('5. Exit')
 
-  
-option = int(input("Select an operation to perform:"))
+choice = int(input("Select an operation to perform"))
 
-    
-if (option in [1, 2, 3, 4]):
-    num1 = int(input('Enter first number:'))
-    num2 = int(input('Enter second number:'))
+if choice == 5:
+    print('Exiting the calculator. Goodbye!')
 
-    if option == 1:
-        result = num1 + num2
-    elif option == 2:
-        result = num1 - num2
-    elif option == 3:
-        result = num1 * num2
-    elif option == 4:
-        result = num1 / num2  
+else:
+
+    a = float(input("Enter First number:"))
+    b = float(input("Enter Second number:"))
+
+if choice == 1:
+    print('Result:', Addition(a, b))
+
+elif choice == 2:
+    print('Result:', subdcription(a, b))
+
+elif choice == 3:
+    print('Result:', multiplication(a, b))
+
+elif choice == 4:
+    print('Result:', division(a, b))
+
 else:
     print('Invalid Operator Selected')
 
 
-print('The Result of the operation is:{}'.format(result))
+    
+
